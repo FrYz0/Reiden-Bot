b0980a9d94375e2fdd50abbb9db0f2f1419455ce
MIMIGAMING83 <46751791+MIMIGAMING83@users.noreply.github.com>
2019-01-16 21:16:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require('discord.js')
+const client = new Discord.Client()
 var prefix = "?";
 
-client.login(process.env.TOKEN);
+client.login(process.env.TOKEN)
 
 client.on('message', message =>{
     if (message.content.startsWith('https://discord.gg/')){
-        message.delete()
         message.reply('Les pubs Discord sont interdites sur ce serveur !')
+        message.delete()
       }
-});
+
+      if (message.content === ('https://discord.gg/')){
+      message.reply('Les pubs Discord sont interdites !')
+      message.delete()
+    }
+})
