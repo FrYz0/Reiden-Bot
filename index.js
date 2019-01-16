09a9e556e1647d5346ac003d2d58f810c5b81ed7
MIMIGAMING83 <46751791+MIMIGAMING83@users.noreply.github.com>
2019-01-16 23:24:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-var prefix = "?";
+var prefix = ("*")
 
 client.login(process.env.TOKEN)
 
@@ -9,7 +9,7 @@ client.on('message', message =>{
         message.reply('Les pubs Discord sont interdites sur ce serveur !')
         message.delete()
       }
-
-    if (message.content === '@Reiden Bot Bravo !'){
-        message.reply('Merci je ne fait que mon travail. :wink:')}
+    if (message.content === prefix /- "clear" ){
+        message.channel.bulkdelete(list)
+    }
 })
