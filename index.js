b461ddf4da8f846703a0862b783d1b79edf74999
MIMIGAMING83 <46751791+MIMIGAMING83@users.noreply.github.com>
2019-01-16 20:38:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ bot.login(process.env.TOKEN)
 
 bot.on('message', message =>{
     if (message.content.startsWith('https://discord.gg/')){
-        message.delete()
         message.reply("Les Pub discord sont interdites. :wink:")
+        message.delete()
       }
 });
