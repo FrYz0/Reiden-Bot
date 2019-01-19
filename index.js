4491b492e3c85886f75d39c0f17e38db73cc5f68
MIMIGAMING83 <46751791+MIMIGAMING83@users.noreply.github.com>
2019-01-19 17:08:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-var prefix = ("*")
+var prefix = "?"
 
-client.login(process.env.TOKEN)
+client.login('process.ev.TOKEN')
 
 client.on('message', message =>{
     if (message.content.startsWith('https://discord.gg/')){
-        message.reply('Les pubs Discord sont interdites sur ce serveur !')
         message.delete()
-      }
-    if (message.content.startsWith("Bravo Reiden Bot !")){
-        message.reply("Merci beacoup ! Je ne fais que mon boulôt :wink:")
+        message.reply('Les pubs Discord sont interdites sur ce serveur !')
     }
-    if (message.content.startsWith(prefix /-"RH.Clear")){
-        if (!message.guild.memeber(message.author).hasPermission("MANAGE_MESSAGE")) return message.channel.reply("Vous n'avez pas la permission:MANAGE_MESSAGE.")
-
-        let args = message.content.split(" ").slice(1)
-
-        if (!args[0]) return message.channel.reply("La commande ne peut pas etre executer pour cause: nombre de message a supprimer manquant")
-        message.channel.bulkDelete(args[0]).then(() => {
-            message.channel.send('${args[0]} Message ont été supprimer.' )
-        })
+    if (message.content.startsWith('http://discord.gg/')){
+        message.delete()
+        message.reply("Et non j'ai penser aux http quand j'ai codé")
     }
 })
